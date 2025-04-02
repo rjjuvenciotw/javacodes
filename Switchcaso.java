5d39a4ddb4abf0a27ccc2134b3a44a17da8e55af
Ricardo Miguel <86057517+rjjuvenciotw@users.noreply.github.com>
2025-04-01 21:26:26 -0300

    Create Switchcaso.java

---
+++ Switchcaso.java
@@ -0,0 +1,17 @@
+public class UsoSwitch
+{
+    public static void main (String args[])
+    {
+        for (int i = 1;i<5; i++)
+        { 
+            switch (i)
+            {
+                case 1: System.out.println ("Numero UM");
+                    break;
+                case 2: System.out.println ("Numero DOIS");
+                    break;
+                case 3: System.out.println ("Numero TRES");
+                    break;
+                default: System.out.println ("NUMERO INVALIDO");
+            }
+    } } } 
